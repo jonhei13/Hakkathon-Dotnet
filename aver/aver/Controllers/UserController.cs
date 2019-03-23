72f611a134204d1ca43f0ba6913021a49a3dfc53
Ólöf Magnúsdóttir <olof.frida92@gmail.com>
2019-03-23 20:32:38 +0000

    User View

---
+++ aver/aver/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using aver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aver.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Hisroty
+        public ActionResult Index()
+        {
+            UserViewModel model = new UserViewModel();
+            model.Items = GetItems();
+            return View(model);
+        }
+
+        public List<UserItem> GetItems()
+        {
+            List<UserItem> items = new List<UserItem>();
+
+            items.Add(new UserItem { Date = DateTime.Now, Title ="Flottur titill"});
+            items.Add(new UserItem { Date = DateTime.Now, Title = "Flottur titill" });
+            items.Add(new UserItem { Date = DateTime.Now, Title = "Flottur titill" });
+            items.Add(new UserItem { Date = DateTime.Now, Title = "Flottur titill" });
+
+            items.Add(new UserItem { Date = DateTime.Now, Title = "Flottur titill" });
+            return items;
+        }
+    }
+}
---
+++ aver/aver/Models/UserItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aver.Models
+{
+    public class UserItem
+    {
+        public List<string> Allergies { get; set; }
+        public String Name { get; set; }
+        public String BloodPressure { get; set; }
+        public int Budget{ get; set; }
+
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+    }
+}
