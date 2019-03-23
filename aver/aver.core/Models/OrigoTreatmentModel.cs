47f02d7ef8cb673bebbdf4bc18f2b2ffd0bb2f28
Alex Ivarsson <alex@fuglar.com>
2019-03-23 12:59:58 +0000

    added treatmentModel

---
+++ aver/aver.core/Models/OrigoTreatmentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aver.core.Models
+{
+    public class OrigoTreatmentModel
+    {
+        public string Description { get; set; }
+        public string Form { get; set; }
+        public double Strenght { get; set; }
+        public string TrengthUnit { get; set; }
+        public double MorningDose { get; set; }
+        public double AfternoonDose { get; set; }
+        public double EveningDose { get; set; }
+        public string Instructions { get; set; }
+    }
+}
+ 
