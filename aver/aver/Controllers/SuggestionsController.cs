7be739569e9331444c558cbed8d0f101c2e1189f
Ari Freyr Guðmundsson <ari.freyr.gudmundsson@advania.is>
2019-03-23 18:08:37 +0000

    icd10 startings

--- aver/aver/Controllers/SuggestionsController.cs
+++ aver/aver/Controllers/SuggestionsController.cs
@@ -26,12 +26,25 @@ namespace aver.Controllers
         }
 
 
-        public string SuggestionTable()
+        public List<SuggestionItem> SuggestionTable()
         {
             List<SuggestionItem> suggestions = new List<SuggestionItem>();
+            suggestions.Add(CreateSuggestionItem("colorectal cancer",50,74,2, 24));
+            suggestions.Add(CreateSuggestionItem("High blood pressure", 18, 99, 2, 24));
+            suggestions.Add(CreateSuggestionItem("High cholesterol", 45, 99, 5,60));
+            suggestions.Add(CreateSuggestionItem("Cardiovascular", 45, 99, 0, 6));
+            suggestions.Add(CreateSuggestionItem("Dental health", 50, 74, 2,24));
+            suggestions.Add(CreateSuggestionItem("Diabetes: type 2", 40, 99, 3, 36));
+            suggestions.Add(CreateSuggestionItem("Glaucoma", 50, 99, 1,12));
+            suggestions.Add(CreateSuggestionItem("Hearing impairment", 65, 99, 1,12));
+            suggestions.Add(CreateSuggestionItem("Overweight", 18, 99, 2,24));
 
+            return suggestions;
+        }
 
-            return "";
+        public SuggestionItem CreateSuggestionItem(string affl, int lower, int upper,int frequencyYears, int frequencyMonths)
+        {
+            return new SuggestionItem() { Affliction = affl, AgeUpper = upper, AgeLower = lower, FrequencyInYears = frequencyYears, FrequencyInMonths = frequencyMonths };
         }
 
         
