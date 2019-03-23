41f238a3172a97fbdfdd4c0a65fc69877a707df7
Ari Freyr Guðmundsson <ari.freyr.gudmundsson@advania.is>
2019-03-23 13:09:05 +0000

    checkin

--- aver/aver/Controllers/NotificationsController.cs
+++ aver/aver/Controllers/NotificationsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aver.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,27 @@ namespace aver.Controllers
         // GET: Notifications
         public ActionResult Index()
         {
-            return View();
+            NotificationViewModel model = new NotificationViewModel();
+
+            model.Items = GetItems();
+            return View(model);
         }
+
+        public List<NotificationItem> GetItems()
+        {
+            List<NotificationItem> items = new List<NotificationItem>();
+
+            items.Add(new NotificationItem { Date =  DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu"});
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            items.Add(new NotificationItem { Date = DateTime.Now, Title = "Uppfærði skýrslu", Desctiption = "Bætti við lýsingu og viðhengi á skýrslu" });
+            return items;
+        }
+
+        
     }
 }
---
+++ aver/aver/Models/NotificationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aver.Models
+{
+    public class NotificationViewModel
+    {
+        public List<NotificationItem> Items { get; set; }
+    }
+
+    public class NotificationItem
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Desctiption { get; set; }
+    }
+}
